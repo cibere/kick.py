79fd8f0a55413feb0cffabea977b25be34976fe2
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-04-22 14:48:14 -0700

    start, code, aenter, aexit for client

--- kick/client.py
+++ kick/client.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
 import asyncio
+from types import TracebackType
+from typing import TYPE_CHECKING, Type
 
 from .http import HTTPClient
 from .message import Message
 from .user import User
 
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
 
 class Client:
     def __init__(self, token: str) -> None:
-        self.http = HTTPClient(token)
+        self.http = HTTPClient(token, self)
 
     async def get_user(self, streamer: str) -> User:
         data = await self.http.get_user(streamer)
@@ -27,4 +32,20 @@ class Client:
             )
 
     async def on_message(self, msg: Message) -> None:
-        ...
+        print(f"Received message from {msg.author.username}")
+
+    async def start(self) -> None:
+        ws = await self.http.create_ws()
+        await ws.start()
+
+    async def close(self) -> None:
+        await self.http.close()
+
+    async def __aenter__(self) -> Self:
+        await self.start()
+        return self
+
+    async def __aexit__(
+        self, exc_type: Type[Exception], exc: Exception, tb: TracebackType
+    ) -> None:
+        await self.close()
