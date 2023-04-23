e60eb17262850dac991635c797a0134d13480781
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-04-22 19:21:56 -0700

    dispatch system + events

--- kick/client.py
+++ kick/client.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-from types import TracebackType
-from typing import TYPE_CHECKING, Type
+from typing import TYPE_CHECKING, Any, Callable, Coroutine, TypeVar
 
 from .http import HTTPClient
 from .message import Message
@@ -11,18 +10,20 @@ from .user import User
 if TYPE_CHECKING:
     from typing_extensions import Self
 
+EventT = TypeVar("EventT", bound=Callable[..., Coroutine[Any, Any, None]])
+
 
 class Client:
     def __init__(self, token: str) -> None:
         self.http = HTTPClient(token, self)
 
-    async def get_user(self, streamer: str) -> User:
+    async def fetch_user(self, streamer: str) -> User:
         data = await self.http.get_user(streamer)
         user = User(data=data)
         user.http = self.http
         return user
 
-    async def dispatch(self, event_name: str, *args, **kwargs) -> None:
+    def dispatch(self, event_name: str, *args, **kwargs) -> None:
         event_name = f"on_{event_name}"
 
         event = getattr(self, event_name, None)
@@ -31,21 +32,24 @@ class Client:
                 event(*args, **kwargs), name=f"event-dispatch: {event_name}"
             )
 
-    async def on_message(self, msg: Message) -> None:
-        print(f"Received message from {msg.author.username}")
+    def event(self, coro: EventT) -> EventT:
+        setattr(self, coro.__name__, coro)
+        return coro
 
     async def start(self) -> None:
-        ws = await self.http.create_ws()
-        await ws.start()
+        await self.http.start()
 
     async def close(self) -> None:
         await self.http.close()
 
     async def __aenter__(self) -> Self:
-        await self.start()
         return self
 
-    async def __aexit__(
-        self, exc_type: Type[Exception], exc: Exception, tb: TracebackType
-    ) -> None:
+    async def __aexit__(self, exc_type, exc, tb) -> None:
         await self.close()
+
+    async def on_ready(self) -> None:
+        ...
+
+    async def on_message(self, msg: Message) -> None:
+        ...
