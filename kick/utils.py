82d8733ce61b379abd3458a1049d1846c8f68dc5
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-04-22 19:20:43 -0700

    add MISSING

---
+++ kick/utils.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+__all__ = ("MISSING",)
+
+
+class _MissingSentinel:
+    __slots__ = ()
+
+    def __eq__(self, other) -> bool:
+        return False
+
+    def __bool__(self) -> bool:
+        return False
+
+    def __hash__(self) -> int:
+        return 0
+
+    def __repr__(self):
+        return "..."
+
+
+MISSING: Any = _MissingSentinel()
