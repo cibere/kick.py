2a654e7d5232495eb63fc600a3806f7f37a9eaba
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-19 16:42:41 -0700

    fix login

--- kick/http.py
+++ kick/http.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import asyncio
 import json
 import logging
-from typing import TYPE_CHECKING, Any, Coroutine, TypeVar, Union, reveal_type
+from typing import TYPE_CHECKING, Any, Coroutine, TypeVar, Union
 
 from aiohttp import ClientResponse, ClientSession
 
@@ -58,11 +58,10 @@ class Route:
     DOMAIN: str = "https://kick.com"
     BASE: str = f"{DOMAIN}/api/v2"
 
-    def __init__(self, method: str, path: str, referrer: str = "/") -> None:
+    def __init__(self, method: str, path: str) -> None:
         self.path: str = path
         self.method: str = method
         self.url = self.BASE + self.path
-        self.referrer = self.DOMAIN + referrer
 
 
 class HTTPClient:
@@ -73,8 +72,11 @@ class HTTPClient:
 
         self.token: str = MISSING
         self.xsrf_token: str = MISSING
+
         self.user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        self.cookie_url = "https://kick.com"
+
+        self.bypass_port = client._options.get("bypass_port", 9090)
+        self.whitelisted = client._options.get("whitelisted", False)
 
     async def close(self) -> None:
         LOGGER.info("Closing HTTP Client...")
@@ -83,15 +85,53 @@ class HTTPClient:
         if self.ws is not MISSING:
             await self.ws.close()
 
-    async def login(self, *, username: str, password: str) -> None:
-        route = Route("POST", "/login")
-        route.url = route.DOMAIN + "/login"
+    async def login(
+        self, *, username: str, password: str, one_time_password: str | None
+    ) -> None:
+        LOGGER.info("Logging in using username and password")
+
+        # Mobile login method is used here since more is known about
+        # how that works compared to the desktop version.
+        # As for compatibility, there is no known endpoints that
+        # a mobile token can not authorize at.
+
+        token_route = Route("GET", "")
+        token_route.url = token_route.DOMAIN + "/kick-token-provider"
+        token_provider = await self.request(token_route)
+
+        with open("token.json", "w") as f:
+            json.dump(token_provider, f, indent=4)
+
+        route = Route("POST", "")
+        route.url = route.DOMAIN + "/mobile/login"
+
         data = {
             "email": username,
             "password": password,
+            "isMobileRequest": True,
+            token_provider["nameFieldName"]: "",
+            token_provider["validFromFieldName"]: token_provider["encryptedValidFrom"],
         }
-        data = await self.request(route, json=data)
-        print(data)
+        if one_time_password is not None:
+            data["one_time_password"] = one_time_password
+
+        res = await self.request(route, json=data)
+        if res["2fa_required"] is True:
+            two_fa_code = input(
+                "[WARNING] 2FA is enabled. Either disable it or give a 2fa code.\n> "
+            )
+            if not two_fa_code:
+                raise LoginFailure("2FA is enabled.")
+            else:
+                data["one_time_password"] = two_fa_code
+                res = await self.request(route, json=data)
+                if res["2fa_required"] is True:
+                    raise LoginFailure("2FA is enabled.")
+        if "message" in res.keys():
+            raise LoginFailure(res["message"])
+
+        self.token = res["token"]
+        LOGGER.info("Successfully logged in")
 
     async def start(self) -> None:
         if self.__session is MISSING:
@@ -102,7 +142,6 @@ class HTTPClient:
         )
         self.ws = ChatroomWebSocket(actual_ws, http=self)
         self.client.dispatch("ready")
-        print("dispatched ready")
         await self.ws.start()
 
     async def request(self, route: Route, **kwargs) -> Any:
@@ -110,9 +149,6 @@ class HTTPClient:
             self.__session = ClientSession()
 
         headers = kwargs.pop("headers", {})
-        headers["Referrer"] = route.referrer
-        headers["Connection"] = "keep-alive"
-        headers["Alt-Used"] = "kick.com"
         headers["User-Agent"] = self.user_agent
 
         cookies = kwargs.pop("cookies", {})
@@ -122,7 +158,6 @@ class HTTPClient:
             cookies["XSRF-TOKEN"] = self.xsrf_token
         if self.token:
             headers["Authorization"] = f"Bearer {self.token}"
-            cookies["kick_session"] = self.token
 
         url = route.url
         endpoint = f"/{route.method.split('/')[-1]}"
@@ -132,24 +167,23 @@ class HTTPClient:
 
         res: ClientResponse | None = None
         data: str | dict | None = None
-        # try:
+
         for current_try in range(3):
             LOGGER.debug(
-                f"Making request to {url}. headers: {headers}, params: {kwargs.get('params', None)}, json: {kwargs.get('json', None)}"
+                f"Making request to {route.method} {url}. headers: {headers}, params: {kwargs.get('params', None)}, json: {kwargs.get('json', None)}"
             )
 
             res = await self.__session.request(
                 route.method,
-                f"http://localhost:9090/request?url={url}",
+                f"http://localhost:{self.bypass_port}/request?url={url}"
+                if self.whitelisted is False
+                else url,
                 headers=headers,
                 cookies=cookies,
                 **kwargs,
             )
 
             if res is not None:
-                self.token = getattr(
-                    res.cookies.get("kick_session", MISSING), "value", MISSING
-                )
                 self.xsrf_token = getattr(
                     res.cookies.get("XSRF-TOKEN", MISSING), "value", MISSING
                 )
@@ -191,12 +225,7 @@ class HTTPClient:
                         raise InternalKickException(txt)
                     case other:
                         raise RuntimeError(f"Unknown status reached: {other}")
-        # finally:
-        #     print("finally triggered")
-        #     if page is not None:
-        #         print("Closing page...")
-        #         await page.close()
-        print("Done2")
+
         if res is not None and data is not None:
             txt = await error_or_text(data)
 
@@ -205,10 +234,7 @@ class HTTPClient:
 
             raise HTTPException(txt)
 
-        raise RuntimeError(
-            f"Unreachable situation occured in http handling. Res: {res}, data: {data}"
-        )
-        # raise RuntimeError("Unreachable situation occured in http handling")
+        raise RuntimeError("Unreachable situation occured in http handling")
 
     def send_message(self, chatroom: int, content: str) -> Response[MessageSentPayload]:
         # raise RuntimeError("This is broky")
@@ -221,15 +247,12 @@ class HTTPClient:
         )
 
     def get_user(self, streamer: str) -> Response[UserPayload]:
-        return self.request(
-            Route(method="GET", path=f"/channels/{streamer}", referrer=f"/{streamer}")
-        )
+        return self.request(Route(method="GET", path=f"/channels/{streamer}"))
 
     def get_chatter(self, streamer: str, chatter: str) -> Response[ChatterPayload]:
         return self.request(
             Route(
                 method="GET",
                 path=f"/channels/{streamer}/users/{chatter}",
-                referrer=f"https://kick.com/{streamer}",
             )
         )
