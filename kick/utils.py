ac5594a9bd9417213167cbc99f22895bb4d68691
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-19 21:49:04 -0700

    + cached_property

--- kick/utils.py
+++ kick/utils.py
@@ -1,6 +1,27 @@
-from typing import Any
+from __future__ import annotations
 
-__all__ = ("MISSING",)
+from functools import wraps
+from typing import TYPE_CHECKING, Any, Callable, Type
+
+
+def _cached_property(func: Callable):
+    @wraps(func)
+    def getter(parent: Type):
+        cache = getattr(parent, "__cached_properties", {})
+        if func.__name__ not in cache.keys():
+            cache[func.__name__] = func(parent)
+            parent.__cached_properties = cache
+        return parent.__cached_properties[func.__name__]
+
+    return property(getter)
+
+
+if TYPE_CHECKING:
+    from functools import cached_property as cached_property
+else:
+    cached_property = _cached_property
+
+__all__ = ("MISSING", "cached_property")
 
 
 class _MissingSentinel:
