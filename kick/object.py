d1f143a0b0f9734657c2888dc4e0848ad28a8241
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-20 09:54:18 -0700

    + HTTPDataclass

--- kick/object.py
+++ kick/object.py
@@ -1,13 +1,28 @@
-from typing import Generic, TypeVar
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Generic, TypeVar
 
 DataT = TypeVar("DataT")
 
-__all__ = ("BaseDataclass",)
+if TYPE_CHECKING:
+    from .http import HTTPClient
+
+
+__all__ = ("BaseDataclass", "HTTPDataclass")
 
 
 class BaseDataclass(Generic[DataT]):
     def __init__(self, *, data: DataT) -> None:
         self._data = data
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}>"
+
+
+class HTTPDataclass(Generic[DataT]):
+    def __init__(self, *, data: DataT, http: HTTPClient) -> None:
+        self._data = data
+        self.http = http
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__}>"
