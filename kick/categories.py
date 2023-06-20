bb432ba85b4bc8bd4f42922327c6628d47e519ea
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-20 10:32:50 -0700

    add docstrings to Client, Credentials, Category, and ParentCategory

--- kick/categories.py
+++ kick/categories.py
@@ -15,18 +15,34 @@ __all__ = ("Category", "ParentCategory")
 class ParentCategory(BaseDataclass["ParentCategoryPayload"]):
     @property
     def id(self) -> int:
+        """
+        The categorie's ID
+        """
+
         return self._data["id"]
 
     @property
     def name(self) -> str:
+        """
+        The categorie's name
+        """
+
         return self._data["name"]
 
     @property
     def slug(self) -> str:
+        """
+        The categorie's slug
+        """
+
         return self._data["slug"]
 
     @property
     def icon(self) -> str:
+        """
+        The categorie's icon url
+        """
+
         return self._data["icon"]
 
     def __eq__(self, other: Any) -> bool:
@@ -36,35 +52,68 @@ class ParentCategory(BaseDataclass["ParentCategoryPayload"]):
 class Category(BaseDataclass["CategoryPayload"]):
     @property
     def id(self) -> int:
+        """
+        The categorie's ID?
+
+        Unknown on the difference between this and `Category.category_id`
+        """
+
         return self._data["id"]
 
     @property
     def category_id(self) -> int:
+        """
+        The categorie's ID?
+
+        Unknown on the difference between this and `Category.id`
+        """
+
         return self._data["category_id"]
 
     @property
     def name(self) -> str:
+        """
+        The categorie's name
+        """
+
         return self._data["name"]
 
     @property
     def slug(self) -> str:
+        """
+        The categorie's slug
+        """
+
         return self._data["slug"]
 
     @property
     def tags(self) -> list[str]:
+        """
+        A list of the categorie's tags
+        """
+
         return self._data["tags"]
 
     @property
     def description(self) -> str | None:
+        """
+        The categorie's description, if any
+        """
+
         return self._data["description"]
 
     @property
     def deleted_at(self) -> Any:
         """THIS IS RAW DATA, UNKNOWN ON WHAT IT RETURNS"""
+
         return self._data["deleted_at"]
 
     @cached_property
     def parent(self) -> ParentCategory:
+        """
+        The categorie's parent category.
+        """
+
         return ParentCategory(data=self._data["category"])
 
     def __eq__(self, other: Any) -> bool:
