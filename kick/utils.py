98dc71a9f11b5720743c10d64f1f44dcc49d81ca
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-20 16:15:49 -0700

    add utils.setup_logging

--- kick/utils.py
+++ kick/utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from functools import wraps
 from typing import TYPE_CHECKING, Any, Callable, Type
 
@@ -21,7 +22,7 @@ if TYPE_CHECKING:
 else:
     cached_property = _cached_property
 
-__all__ = ("MISSING", "cached_property")
+__all__ = ("MISSING", "cached_property", "setup_logging")
 
 
 class _MissingSentinel:
@@ -41,3 +42,83 @@ class _MissingSentinel:
 
 
 MISSING: Any = _MissingSentinel()
+
+
+class _ColourFormatter(logging.Formatter):
+    # ANSI codes are a bit weird to decipher if you're unfamiliar with them, so here's a refresher
+    # It starts off with a format like \x1b[XXXm where XXX is a semicolon separated list of commands
+    # The important ones here relate to colour.
+    # 30-37 are black, red, green, yellow, blue, magenta, cyan and white in that order
+    # 40-47 are the same except for the background
+    # 90-97 are the same but "bright" foreground
+    # 100-107 are the same as the bright ones but for the background.
+    # 1 means bold, 2 means dim, 0 means reset, and 4 means underline.
+
+    LEVEL_COLOURS = [
+        (logging.DEBUG, "\x1b[40;1m"),
+        (logging.INFO, "\x1b[34;1m"),
+        (logging.WARNING, "\x1b[33;1m"),
+        (logging.ERROR, "\x1b[31m"),
+        (logging.CRITICAL, "\x1b[41m"),
+    ]
+
+    FORMATS = {
+        level: logging.Formatter(
+            f"\x1b[30;1m%(asctime)s\x1b[0m {colour}%(levelname)-8s\x1b[0m \x1b[35m%(name)s\x1b[0m %(message)s",
+            "%Y-%m-%d %H:%M:%S",
+        )
+        for level, colour in LEVEL_COLOURS
+    }
+
+    def format(self, record):
+        formatter = self.FORMATS.get(record.levelno)
+        if formatter is None:
+            formatter = self.FORMATS[logging.DEBUG]
+
+        # Override the traceback to always print in red
+        if record.exc_info:
+            text = formatter.formatException(record.exc_info)
+            record.exc_text = f"\x1b[31m{text}\x1b[0m"
+
+        output = formatter.format(record)
+
+        # Remove the cache layer
+        record.exc_text = None
+        return output
+
+
+def setup_logging(
+    *,
+    handler: logging.Handler = MISSING,
+    formatter: logging.Formatter = MISSING,
+    level: int = MISSING,
+    root: bool = True,
+    stream_supports_colour: bool = False,
+) -> None:
+    if level is MISSING:
+        level = logging.INFO
+
+    if handler is MISSING:
+        handler = logging.StreamHandler()
+
+    if formatter is MISSING:
+        if (
+            isinstance(handler, logging.StreamHandler)
+            and stream_supports_colour is True
+        ):
+            formatter = _ColourFormatter()
+        else:
+            dt_fmt = "%Y-%m-%d %H:%M:%S"
+            formatter = logging.Formatter(
+                "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+            )
+
+    if root:
+        logger = logging.getLogger()
+    else:
+        library, _, _ = __name__.partition(".")
+        logger = logging.getLogger(library)
+
+    handler.setFormatter(formatter)
+    logger.setLevel(level)
+    logger.addHandler(handler)
