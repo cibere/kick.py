9eaf231c7931013baed06541fc09ac191d6a2e7a
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-20 16:21:04 -0700

    add examples

---
+++ examples/receive_message.py
@@ -0,0 +1,22 @@
+from kick import Client, Message
+
+client = Client()
+
+
+@client.event
+async def on_message(msg: Message):
+    print(f"Received message by {msg.author.username} saying {msg.content}")
+
+
+@client.event
+async def on_ready():
+    print("I have successfully logged in")
+
+    username = "xqc"  # The username/slug of the user's chat you want to listen to
+    user = await client.fetch_user(username)
+    await user.chatroom.connect()
+
+
+# You can also pass the `credentials` arg to authenticate yourself
+# Authentication is not required for listening to messages
+client.run()
