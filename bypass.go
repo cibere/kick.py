0854b665e0bb1b573735014460bc4d383503c18a
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-21 21:42:11 -0700

    add proxy option to `kick bypass create`

--- bypass.go
+++ bypass.go
@@ -29,8 +29,7 @@ func sendKickMessage(context *gin.Context) cycletls.Response {
 		Headers:   headers,
         Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
         UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
-	// DO NOT USE ROTATING PROXY
-        // Proxy: "http://username:password@hostname.com:port",
+        // PROXY_HERE
     }, context.Request.Method)
     if err != nil {
         log.Print("Request Failed: " + err.Error())
