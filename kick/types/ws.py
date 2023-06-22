d88949fc2d01b959f679df883a3b8d157c51a825
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-06-22 11:50:04 -0700

    move PartialLivestream to kick/types/ws.py

---
+++ kick/types/ws.py
@@ -0,0 +1,20 @@
+from typing_extensions import TypedDict
+
+from .assets import AssetSrcset, AssetUrl
+from .categories import Category
+
+
+class PartialLivestreamPayload(TypedDict):
+    id: int
+    channel_id: int
+    session_title: str
+    source: None  # Unknown
+    created_at: str
+
+
+class FollowersUpdatePayload(TypedDict):
+    followersCount: str
+    channel_id: int
+    username: None | str
+    created_at: int
+    followed: bool
